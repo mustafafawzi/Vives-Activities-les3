1cb485eaf109ff4856c6ec2450a76f52e83f61bf
mustafafawzi <114569862+mustafafawzi@users.noreply.github.com>
2023-10-06 12:11:09 +0200

    Initial commit

---
+++ VivesActivities.Ui.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VivesActivities.Ui.Mvc.Models;
+
+namespace VivesActivities.Ui.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
